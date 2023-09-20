cae265a0faebc763beda0bd6853800b5412c0061
Azhar Rudin <azharrudin0905@gmail.com>
2023-09-20 09:16:51 +0700

    react installation

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -1,30 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
 
-function Example() {
+function ProductCard() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios.get('/items/fetch')
+        .then((response) => {
+          setData(response.data); // Update the state with fetched data
+        })
+    },[]);
     return (
         <div className="container">
             <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
+            {data.map((item) => (
+                <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  sx={{ height: 140 }}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Share</Button>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+              ))}
+             </div>
         </div>
     );
 }
 
-export default Example;
+export default ProductCard;
 
 if (document.getElementById('example')) {
     const Index = ReactDOM.createRoot(document.getElementById("example"));
 
     Index.render(
         <React.StrictMode>
-            <Example/>
+            <ProductCard/>
         </React.StrictMode>
     )
 }
