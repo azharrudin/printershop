02abddb9c9fb4d66675e21db1dc84c00e9162201
Azhar Rudin <azharrudin0905@gmail.com>
2023-09-20 11:11:57 +0700

    update

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -33,16 +33,16 @@ function ProductCard() {
                 />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
-                    Lizard
+                    {item["namaprinter"]}
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
+                  {item["spesifikasi"]}
+
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
+                  <Button size="small" variant='body2'>{item["harga"].split("000").join(".")}</Button>
+                  <Button size="small">Tambah ke keranjang</Button>
                 </CardActions>
               </Card>
               ))}
